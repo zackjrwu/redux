2024649b9b42d169c7543a48a48fcc325b54e547
zack <holyzack1123@hotmail.com>
2023-11-10 01:34:14 +0800

    Add haha

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React HAHA
         </a>
         <MyComponent />
         <AnotherComponent />
