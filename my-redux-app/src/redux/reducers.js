e8cc2d0dc505249ffd7b9a158a72a6bffa562af6
zack <holyzack1123@hotmail.com>
2023-11-10 01:36:28 +0800

    can not below 0, action log

--- my-redux-app/src/redux/reducers.js
+++ my-redux-app/src/redux/reducers.js
@@ -5,8 +5,14 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
+      console.log("Incrementing");
       return { count: state.count + 1 };
     case "DECREMENT":
+      if (state.count - 1 < 0) {
+        console.log("Cannot decrement below 0");
+        return state;
+      }
+      console.log("Decrementing");
       return { count: state.count - 1 };
     default:
       return state;
